7561cf5400463b387ed0731da90411ae5df2bb03
erik9631 <erik9631@gmail.com>
2024-05-11 12:46:00 +0200

    feat: Added more patch tests fix: Fixed includes in tests

--- test/MemoryTests.cpp
+++ test/MemoryTests.cpp
@@ -1,12 +1,15 @@
 //
 // Created by erik9 on 5/10/2024.
 //
+#include "catch_amalgamated.hpp"
+
+#include <iostream>
+#include "memory/Memory.h"
+#include "algorithms/KmpSearcher.h"
+#include "utils/Utils.h"
+
 #include <windows.h>
 #include <psapi.h>
-#include <iostream>
-
-#include "catch_amalgamated.hpp"
-#include "memory/Memory.h"
 
 int patchableFunctionTest(int iterations, int b) {
     for(int i = 0; i < iterations; i++) {
@@ -28,4 +31,23 @@ TEST_CASE("Basic patch load test", "[PatchTest]") {
     REQUIRE(operationFunction(1, 2) == 5151);
     REQUIRE(operationFunction(5, 8) == 5670);
     REQUIRE(operationFunction(781, 123) == 98231);
+}
+
+
+TEST_CASE("Basic patch test", "[Patchtest2]") {
+    unsigned char pattern[] = {0x89, 0x54, 0x24, 0x10, 0x89, 0x4C, 0x24, 0x08, 0x57, 0x48, 0x83, 0xEC, 0, 0xC7, 0x04, 0x24, 0, 0, 0, 0, 0xEB, 0};
+    HMODULE appModuleHandle = GetModuleHandle(nullptr);
+    HANDLE processHandle = GetCurrentProcess();
+    MODULEINFO exeInfo;
+    GetModuleInformation(processHandle, appModuleHandle, &exeInfo, sizeof(MODULEINFO));
+    auto startAddress = reinterpret_cast<unsigned char*>(exeInfo.lpBaseOfDll);
+    size_t moduleSize = exeInfo.SizeOfImage;
+
+    algorithms::KmpSearcher searcher{pattern, ArraySize(pattern)};
+    auto results = searcher.ParallelSearch(startAddress, moduleSize);
+    REQUIRE(results.size_approx() == 1);
+
+    algorithms::KmpResult<unsigned char> result{};
+    results.try_dequeue(result);
+    std::cout << std::hex <<"Found at: " << (void*) result.data << std::endl;
 }
