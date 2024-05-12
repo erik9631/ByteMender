8d8cbb133b699917ca8dd09eb004793e0938c66a
erik9631 <erik9631@gmail.com>
2024-05-12 12:30:59 +0200

    feat: Added different proxy method

--- src/proxies/version.h
+++ src/proxies/version.h
@@ -4,155 +4,25 @@
 
 #ifndef VERSION_H
 #define VERSION_H
-#include "proxies/setup.h"
 
 namespace proxies::version {
-    #pragma optimize("", off)
-
-
-    void DUMMY0()
-    {
-        #pragma comment(linker, "/EXPORT:GetFileVersionInfoA=" __FUNCDNAME__)
-        CALL_DUMMY();
-        function8byteFill = 0x9090909090909090;
-    }
-
-    void DUMMY1()
-    {
-        #pragma comment(linker, "/EXPORT:GetFileVersionInfoByHandle=" __FUNCDNAME__)
-        CALL_DUMMY();
-        function8byteFill = 0x9090909090909090;
-    }
-
-    void DUMMY2()
-    {
-        #pragma comment(linker, "/EXPORT:GetFileVersionInfoExA=" __FUNCDNAME__)
-        CALL_DUMMY();
-        function8byteFill = 0x9090909090909090;
-    }
-
-    void DUMMY3()
-    {
-        #pragma comment(linker, "/EXPORT:GetFileVersionInfoExW=" __FUNCDNAME__)
-        CALL_DUMMY();
-        function8byteFill = 0x9090909090909090;
-    }
-
-    void DUMMY4()
-    {
-        #pragma comment(linker, "/EXPORT:GetFileVersionInfoSizeA=" __FUNCDNAME__)
-        CALL_DUMMY();
-        function8byteFill = 0x9090909090909090;
-    }
-
-    void DUMMY5()
-    {
-        #pragma comment(linker, "/EXPORT:GetFileVersionInfoSizeExA=" __FUNCDNAME__)
-        CALL_DUMMY();
-        function8byteFill = 0x9090909090909090;
-    }
-
-    void DUMMY6()
-    {
-        #pragma comment(linker, "/EXPORT:GetFileVersionInfoSizeExW=" __FUNCDNAME__)
-        CALL_DUMMY();
-        function8byteFill = 0x9090909090909090;
-    }
-
-    void DUMMY7()
-    {
-        #pragma comment(linker, "/EXPORT:GetFileVersionInfoSizeW=" __FUNCDNAME__)
-        CALL_DUMMY();
-        function8byteFill = 0x9090909090909090;
-    }
-
-    void DUMMY8()
-    {
-        #pragma comment(linker, "/EXPORT:GetFileVersionInfoW=" __FUNCDNAME__)
-        CALL_DUMMY();
-        function8byteFill = 0x9090909090909090;
-    }
-
-    void DUMMY9()
-    {
-        #pragma comment(linker, "/EXPORT:VerFindFileA=" __FUNCDNAME__)
-        CALL_DUMMY();
-        function8byteFill = 0x9090909090909090;
-    }
-
-    void DUMMY10()
-    {
-    #pragma comment(linker, "/EXPORT:VerFindFileW=" __FUNCDNAME__)
-        CALL_DUMMY();
-        function8byteFill = 0x9090909090909090;
-    }
-
-    void DUMMY11()
-    {
-        #pragma comment(linker, "/EXPORT:VerInstallFileA=" __FUNCDNAME__)
-        CALL_DUMMY();
-        function8byteFill = 0x9090909090909090;
-    }
-
-    void DUMMY12()
-    {
-        #pragma comment(linker, "/EXPORT:VerInstallFileW=" __FUNCDNAME__)
-        CALL_DUMMY();
-        function8byteFill = 0x9090909090909090;
-    }
-
-    void DUMMY13()
-    {
-        #pragma comment(linker, "/EXPORT:VerLanguageNameA=" __FUNCDNAME__)
-        CALL_DUMMY();
-        function8byteFill = 0x9090909090909090;
-    }
-
-    void DUMMY14()
-    {
-        #pragma comment(linker, "/EXPORT:VerLanguageNameW=" __FUNCDNAME__)
-        CALL_DUMMY();
-        function8byteFill = 0x9090909090909090;
-    }
-
-    void DUMMY15()
-    {
-        #pragma comment(linker, "/EXPORT:VerQueryValueA=" __FUNCDNAME__)
-        CALL_DUMMY();
-        function8byteFill = 0x9090909090909090;
-    }
-
-    void DUMMY16()
-    {
-        #pragma comment(linker, "/EXPORT:VerQueryValueW=" __FUNCDNAME__)
-        CALL_DUMMY();
-        function8byteFill = 0x9090909090909090;
-    }
-
-    #pragma optimize("", on)
-
-    static ExportDir versionExport{
-        {
-            {DUMMY0, "GetFileVersionInfoA"},
-            {DUMMY1, "GetFileVersionInfoByHandle"},
-            {DUMMY2, "GetFileVersionInfoExA"},
-            {DUMMY3, "GetFileVersionInfoExW"},
-            {DUMMY4, "GetFileVersionInfoSizeA"},
-            {DUMMY5, "GetFileVersionInfoSizeExA"},
-            {DUMMY6, "GetFileVersionInfoSizeExW"},
-            {DUMMY7, "GetFileVersionInfoSizeW"},
-            {DUMMY8, "GetFileVersionInfoW"},
-            {DUMMY9, "VerFindFileA"},
-            {DUMMY10, "VerFindFileW"},
-            {DUMMY11, "VerInstallFileA"},
-            {DUMMY12, "VerInstallFileW"},
-            {DUMMY13, "VerLanguageNameA"},
-            {DUMMY14, "VerLanguageNameW"},
-            {DUMMY15, "VerQueryValueA"},
-            {DUMMY16, "VerQueryValueW"}
-        },
-        LoadLibraryA("C:/Windows/System32/version.dll")
-    };
-
+    #pragma comment(linker, "/export:GetFileVersionInfoA=version_o.GetFileVersionInfoA")
+    #pragma comment(linker, "/export:GetFileVersionInfoByHandle=version_o.GetFileVersionInfoByHandle")
+    #pragma comment(linker, "/export:GetFileVersionInfoExA=version_o.GetFileVersionInfoExA")
+    #pragma comment(linker, "/export:GetFileVersionInfoExW=version_o.GetFileVersionInfoExW")
+    #pragma comment(linker, "/export:GetFileVersionInfoSizeA=version_o.GetFileVersionInfoSizeA")
+    #pragma comment(linker, "/export:GetFileVersionInfoSizeExA=version_o.GetFileVersionInfoSizeExA")
+    #pragma comment(linker, "/export:GetFileVersionInfoSizeExW=version_o.GetFileVersionInfoSizeExW")
+    #pragma comment(linker, "/export:GetFileVersionInfoSizeW=version_o.GetFileVersionInfoSizeW")
+    #pragma comment(linker, "/export:GetFileVersionInfoW=version_o.GetFileVersionInfoW")
+    #pragma comment(linker, "/export:VerFindFileA=version_o.VerFindFileA")
+    #pragma comment(linker, "/export:VerFindFileW=version_o.VerFindFileW")
+    #pragma comment(linker, "/export:VerInstallFileA=version_o.VerInstallFileA")
+    #pragma comment(linker, "/export:VerInstallFileW=version_o.VerInstallFileW")
+    #pragma comment(linker, "/export:VerLanguageNameA=version_o.VerLanguageNameA")
+    #pragma comment(linker, "/export:VerLanguageNameW=version_o.VerLanguageNameW")
+    #pragma comment(linker, "/export:VerQueryValueA=version_o.VerQueryValueA")
+    #pragma comment(linker, "/export:VerQueryValueW=version_o.VerQueryValueW")
 }
+
 #endif //VERSION_H
